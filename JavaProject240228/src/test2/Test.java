dd8f17a7f0c9040fd8f8eddd0881bbc355549e87
lsy3709 <lsy3709@naver.com>
2024-02-28 12:29:15 +0900

    4교시 저장1, 2번째 커밋 테스트

--- JavaProject240228/src/test2/Test.java
+++ JavaProject240228/src/test2/Test.java
@@ -1,6 +1,14 @@
 package test2;
 
 public class Test {
+	// 파일 명과 클래스명은 동일해야하고, 
+	// 만약, 틀린다면, 문법 검사기(컴파일러) 가 
+	// 오류를 체크 해줍니다. 
+	// 모든 문법를 다 알려고 안해도 된다. 
+	// 결론, 내려놓아라.!!!
+	// 계속 작업을 하다보면, 자연스럽게 스며든다.
+	// 비지니스 로직이라고, 실제로 우리가 구현해야할 기능들에
+	// 알고리즘을 고민을 하는 때가 옵니다. 
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
