1b3e5059303965233d125e8f93555dfa800a48dd
lsy3709 <lsy3709@naver.com>
2024-02-28 10:29:32 +0900

    first commit

---
+++ JavaProject240228/src/ex_240228/Test1.java
@@ -0,0 +1,19 @@
+package ex_240228;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// vscode에서 처럼, UI 폰트, 글자 크기, 자동완성 등. 
+		// 자바스크립트 함수 이름으로 사용했죠?
+		// 자바에서는 이름 : 메서드 로 사용함. 
+		// 정의, 호출(사용)
+		// 역할, 콘솔에 메세지를 출력함. 매우 중요함. 
+		// 왜? 프로그래밍에서, 항상 값을 잘 입력, 출력 되었는지
+		// 반드시 확인해야함. 확인하는 도구로 사용함. 
+		// 실행. 
+		System.out.println("Hellow World.!!!");
+
+	}
+
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+"# JavaPractice240228" 
