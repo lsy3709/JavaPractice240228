8c56d63b337db3619ba8dd6b22ced0380d0d5d8f
lsy3709 <lsy3709@naver.com>
2024-02-29 11:18:22 +0900

    3교시 while 문 활용해서, 더하는 프로그램 계속 진행 및 빠져나가기 예제

--- JavaProject240228/src/ex_240229/Ex_3_for.java
+++ JavaProject240228/src/ex_240229/Ex_3_for.java
@@ -5,11 +5,28 @@ import java.util.Scanner;
 public class Ex_3_for {
 
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		System.out.println(" 입력한 수 n 까지 더하는 프로그램 , 숫자를 입력해주세요>>");
-		int number = scanner.nextInt();
-		int result = sumN(number);
-		System.out.println("결과는 : " + result);
+		// while문은 보통은 무언가 계속 실행하게 하고, 
+		// 특정 조건에서 빠져 나올 때 많이 사용한다. 
+		while (true) {
+			Scanner scanner = new Scanner(System.in);
+			System.out.println("입력한 수 n 까지 더하는 프로그램");
+			System.out.println(" 숫자를 입력해주세요, 0번: 종료시 누르기 >>");
+			int number = scanner.nextInt();
+			
+			// 조건문 분기
+			if (number >= 1) {
+				int result = sumN(number);
+				System.out.println("결과는 : " + result);
+			} else if (number == 0) {
+				System.out.println("프로그램 종료합니다.");
+				break;
+			} else {
+				System.out.println("양수 또는 2번중에서 입력해주세요.");
+			}
+			
+			
+		}
+		
 	}
 	// 입력한 수 n 까지 더하는 프로그램 만들기 : 1+2+3+...+n 
 	public static int sumN(int number) {
