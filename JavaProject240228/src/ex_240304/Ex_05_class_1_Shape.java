a19374b350b8de18ca8e684c37d776c697563c35
lsy3709 <lsy3709@naver.com>
2024-03-04 15:41:18 +0900

    6교시 class 기본 정의 및 사용 , 접근 지정자 protected 상속받은 자식 클래스에서만 접근 확인예제

---
+++ JavaProject240228/src/ex_240304/Ex_05_class_1_Shape.java
@@ -0,0 +1,12 @@
+package ex_240304;
+
+public class Ex_05_class_1_Shape {
+
+	public static void main(String[] args) {
+		Triangle tri1 = new Triangle();
+		int protecedVarString = tri1.edgePoint;
+		
+
+	}
+
+}
---
+++ JavaProject240228/src/ex_240304/Rectangle.java
@@ -0,0 +1,9 @@
+package ex_240304;
+
+public class Rectangle { // shape 상속을 안받고 
+	// 일반 멤버 하나 정도 추가 후, 
+	// 실행 메서드가 있는 클래스 파일에서, 
+	// 부모의 protected 변수에 접근 안되는 부분 확인 하면됨. 
+	
+
+}
