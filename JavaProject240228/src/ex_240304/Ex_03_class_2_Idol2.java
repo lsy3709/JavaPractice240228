fe24e6bd02c1e90f530460f357d74fbe9531704d
lsy3709 <lsy3709@naver.com>
2024-03-04 12:50:49 +0900

    4교시 class 기본 정의 및 사용 , 상속에서 아이돌 부모클래스 상속받은 블랙핑크 클래스 다형성

--- JavaProject240228/src/ex_240304/Ex_03_class_2_Idol2.java
+++ JavaProject240228/src/ex_240304/Ex_03_class_2_Idol2.java
@@ -39,10 +39,38 @@ public class Ex_03_class_2_Idol2 {
 		
 		// 블랙핑크라고 만들었고, 아이돌 클래스를 상속을 받았음. 
 		// 사용하기. 
-		BlackPink blackPink = new BlackPink(groupNameString, groupMember, groupAlbum, areaActivity);
+//		BlackPink blackPink = new BlackPink(groupNameString, groupMember, groupAlbum, areaActivity);
+//		blackPink.introduceMember();
+//		blackPink.introduceAlbum();
+//		blackPink.infoActivityArea();
+		
+		// 다형성 보여주기.
+		// 부모 타입으로 받는다. 작은 집 -> 큰 집으로 갈 때는 문제가 안된다. 
+		// 주의사항, blackPink는 1)아이돌 이면서, 2) 블랙핑크 임. 
+		// 하지만, 타입이 부모 or 자식 ? 부모 이기 때문에, 
+		// 이 인스턴스 실행을하게 되면, 바라보는 레퍼런스, 부모를 보게됩니다. 
+		// 뭔소리? 부모의 기능만 구현이 됩니다. 
+		
+		// 그래서, 본인의 자식 클래스를 사용하고 싶다면, 다운 캐스팅
+		// 부모 클래스 -> 자식 클래스 다운 캐스팅. 함. 
+		Idol blackPink = new BlackPink(groupNameString, groupMember, groupAlbum, areaActivity);
 		blackPink.introduceMember();
 		blackPink.introduceAlbum();
-		blackPink.infoActivityArea();
+		// 자식에서 기능을 하나를 더 추가를 했지만, 객체로 점찍고, 메서드를 확인해보면, 
+		// 자식 클래스에서 정의를 한 메서드가 안보여요. !!
+		// 다형성을 이용해서, 사용을 하지만, 실제 사용은 부모 꺼 밖에 사용을 못함. 
+		
+		// 부모 타입을 다시, 자식 타입으로 변경을 해서, 원래 자식에서 만들었던 활동 지역 기능을 사용해보기. 
+		BlackPink blackPink2 = (BlackPink)blackPink;
+		blackPink2.infoActivityArea();
+		
+		// 다형성으로 왜 하냐? 
+		// 재정의를 할 때, 유연한 작업을 하기위해서, 유지보수를 위해서, 
+		// 상속에서, 모든 클래스들의 최고 부모 클래스 Object 클래스를 
+		// 암묵적으로 다 상속을 받고 있음. 
+		// 부모 - 자식 간에 타입 변환을 용이하게 해서, 유지보수를 편하게 하기 위해서. 
+		
+		
 		scanner.close();
 
 	}
