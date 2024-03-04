93f35b1a3a593f66dbe16cc8a4551478791b4f1d
lsy3709 <lsy3709@naver.com>
2024-03-04 15:56:57 +0900

    7교시 class 기본 정의 및 사용 , 접근 지정자 protected 상속받은 자식 클래스에서만 접근 확인예제2

--- JavaProject240228/src/ex_240304/Ex_05_class_1_Shape.java
+++ JavaProject240228/src/ex_240304/Ex_05_class_1_Shape.java
@@ -3,8 +3,13 @@ package ex_240304;
 public class Ex_05_class_1_Shape {
 
 	public static void main(String[] args) {
+		// 자식 클래스에서, 부모 클래스의 접근 지정자가 protected 이면 보인다. 확인. 
 		Triangle tri1 = new Triangle();
 		int protecedVarString = tri1.edgePoint;
+		
+		// 자식 클래스가 아니예요. 상속을 안받았고, 그래서, protected 안 보인다. 확인.
+		Rectangle rec1 = new Rectangle();
+//		rec1.
 		
 
 	}
