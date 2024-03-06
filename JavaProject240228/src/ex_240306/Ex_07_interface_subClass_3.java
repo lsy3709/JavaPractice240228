2c2429bc97c4bb72aad6fbbe85ffa7bdb3eeda05
lsy3709 <lsy3709@naver.com>
2024-03-06 16:25:36 +0900

    7교시 상속, 인터페이스 정의 및 사용, 클래스 상속도 같이 진행 준비

---
+++ JavaProject240228/src/ex_240306/Ex_07_interface_subClass_3.java
@@ -0,0 +1,30 @@
+package ex_240306;
+
+// 자식 클래스에서 구현한 경우.
+public class Ex_07_interface_subClass_3 implements test1{
+
+	@Override
+	public void showMenu() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void showDrinkMenu() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void showSnackMenu() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+	
+	// 인터페이스를 구현하는 기본 문법 
+	// 클래스 명 implements 인터페이스 명1,인터페이스 명2,
+	
+}
