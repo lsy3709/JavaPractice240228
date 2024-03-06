5536a835147eac18dcce9e3d0c368b7d2836f473
lsy3709 <lsy3709@naver.com>
2024-03-06 16:17:34 +0900

    7교시 상속, 인터페이스 다중 상속 확인 메모

--- JavaProject240228/src/ex_240306/Ex_06_interface_subClass_2.java
+++ JavaProject240228/src/ex_240306/Ex_06_interface_subClass_2.java
@@ -1,13 +1,29 @@
 package ex_240306;
 
 // 자식 클래스에서 구현한 경우.
-public class Ex_06_interface_subClass_2 implements Ex_05_interface_1{
+public class Ex_06_interface_subClass_2 implements test1{
 
 	@Override
-	public void showInfo() {
-		System.out.println("인터페이스를 구현한 하위 클래스에서 showInfo 호출.");
+	public void showMenu() {
+		// TODO Auto-generated method stub
 		
 	}
+
+	@Override
+	public void showDrinkMenu() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void showSnackMenu() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+	
 	// 인터페이스를 구현하는 기본 문법 
 	// 클래스 명 implements 인터페이스 명1,인터페이스 명2,
 	
