28ce5a085fcdfc29e78bb930d4c2bf385870f819
lsy3709 <lsy3709@naver.com>
2024-03-07 11:51:27 +0900

    3교시 응용하기, 등록하는 메서드 인자값으로 빈 스포츠 객체, 나올때는 입력받은 정보를 적용한 객체로 리턴

--- JavaProject240228/src/ex_240307/InputFuncs.java
+++ JavaProject240228/src/ex_240307/InputFuncs.java
@@ -2,9 +2,22 @@ package ex_240307;
 
 import java.util.Scanner;
 
+// 스포츠 해당 종목들이 각각의 인스턴스.
+// Sports 라는 타입으로 배열을 선언을 해서.
+// 이 배열의 요소로 담기. 
+// 담아 놓은 배열에서, 검색을 하거나, 출력 하면 됨. 
+
+
 public class InputFuncs {
 	// 1. 스포츠 등록하는 기능.
-	public static void enrollSports() {
+	
+	// 해당 등록하는 함수에, 입력 타입으로 스포츠 타입의 레퍼런스 변수(인스턴스)
+	// 메서드 입력값 
+	//들어갈 때는 , 빈 인스턴스가 들어가고,
+	// 메서드 반환값
+	// 나올때 는 , 인스턴스에 (콘솔에서 입력한 데이터를 ) 적용한 인스턴스가 나온다. 
+	
+	public static Sports enrollSports(Sports sports) {
 		// 콘솔 입력을 받는 부분
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("스포츠 종목명(문자열로) 입력해주세요>>:");
@@ -40,6 +53,7 @@ public class InputFuncs {
 		// 콘솔에서 입력 받은 데이터를, 우리가 만든 클래스에 적용하기. 
 		
 		
+		return sports;
 	}
 	
 	// 2. 스포츠 검색해서 출력하는 기능. 
