7cff9391b745a3eb59123899858188a4da55888b
lsy3709 <lsy3709@naver.com>
2024-03-08 15:29:23 +0900

    5교시 Math 랜덤 기능 이용해서 로또 생성기 2(배열로만)

--- JavaProject240228/src/ex_240308/Ex_04_Math_1.java
+++ JavaProject240228/src/ex_240308/Ex_04_Math_1.java
@@ -3,7 +3,7 @@ package ex_240308;
 import java.util.Random;
 
 public class Ex_04_Math_1 {
-	static int [] pickedNumber;
+	static int[] pickedNumber;
 
 	public static void main(String[] args) {
 		// Math , 수학 관련된 기능을 많이 제공하고,
@@ -29,55 +29,138 @@ public class Ex_04_Math_1 {
 		// 로또 번호 자동 생성기 만들어보기.
 		// 1 이상 46 미만, * 45 + 1
 		int num2 = (int) (Math.random() * 45 + 1);
-		// 배열로 
+		// 배열로
 		pickedNumber = new int[6];
 		// 난수 생성 메서드로 호출후, 배열에 담기
-		// 그리고, 배열에 이미 숫자가 있다면, 분기문으로 처리하고, 
-		// 중복없이 , 번호 6개 뽑을 때 까지, 반복 처리 : while 
-		
+		// 그리고, 배열에 이미 숫자가 있다면, 분기문으로 처리하고,
+		// 중복없이 , 번호 6개 뽑을 때 까지, 반복 처리 : while
 		int countCheck = 0;
-		while(true) {
-		int pickNumber = generateRandomNumber();
-		for(int i = 0 ; i < pickedNumber.length; i++) {
-			// 기존 배열에 뽑힌 숫자가 없다면 추가.
-			// 기존 배열에 뽑힌 숫자가 있다면 다시 생성.
-			
+		int[] tempInt = new int[6];
+		while (true) {
+			if (countCheck == 6) {
+				System.out.println("로또 번호 생성기 출력");
+				for(int n:tempInt) {
+					System.out.print(n + " ");
+				}
+				System.out.println();
+				System.out.println("종료합니다.");				
+				break;
+			} else if (countCheck == 0) {
 
-			// 중복이 되면 -> 중복이 안될때 까지 검사 후, 배열에 추가	
-			if(pickNumber == pickedNumber[i]) {
-				
-			// 중복이 안되면, 배열에 추가						
-			} else {
-				pickedNumber[i]= pickNumber;
+				int pickNumber = generateRandomNumber();
+				// 중복 검사. true 중복, false 중복 아님.
+				while (true) {
+					if (!checkNumber(pickNumber, tempInt)) {
+						tempInt[countCheck] = pickNumber;
+						countCheck++;
+						break;
+
+					}
+				}
+			} else if (countCheck == 1) {
+				int pickNumber = generateRandomNumber();
+				// 중복 검사. true 중복, false 중복 아님.
+				while (true) {
+					if (!checkNumber(pickNumber, tempInt)) {
+						tempInt[countCheck] = pickNumber;
+						countCheck++;
+						break;
+
+					}
+				}
+
+			} else if (countCheck == 2) {
+				int pickNumber = generateRandomNumber();
+				// 중복 검사. true 중복, false 중복 아님.
+				while (true) {
+					if (!checkNumber(pickNumber, tempInt)) {
+						tempInt[countCheck] = pickNumber;
+						countCheck++;
+						break;
+
+					}
+				}
+
+			}
+
+			else if (countCheck == 3) {
+				int pickNumber = generateRandomNumber();
+				// 중복 검사. true 중복, false 중복 아님.
+				while (true) {
+					if (!checkNumber(pickNumber, tempInt)) {
+						tempInt[countCheck] = pickNumber;
+						countCheck++;
+						break;
+
+					}
+				}
+
 			}
 			
-			countCheck++;
-		}
-		}
-		
+			else if (countCheck == 4) {
+				int pickNumber = generateRandomNumber();
+				// 중복 검사. true 중복, false 중복 아님.
+				while (true) {
+					if (!checkNumber(pickNumber, tempInt)) {
+						tempInt[countCheck] = pickNumber;
+						countCheck++;
+						break;
+
+					}
+				}
+
+			}
+			
+			else if (countCheck == 5) {
+				int pickNumber = generateRandomNumber();
+				// 중복 검사. true 중복, false 중복 아님.
+				while (true) {
+					if (!checkNumber(pickNumber, tempInt)) {
+						tempInt[countCheck] = pickNumber;
+						countCheck++;
+						break;
+
+					}
+				}
+
+			}
+			
+			else if (countCheck == 6) {
+				int pickNumber = generateRandomNumber();
+				// 중복 검사. true 중복, false 중복 아님.
+				while (true) {
+					if (!checkNumber(pickNumber, tempInt)) {
+						tempInt[countCheck] = pickNumber;
+						countCheck++;
+						break;
+
+					}
+				}
+
+			}
+		} // while 문 종료 지점. 
 
 	}
-	// 랜덤하게 번호를 하나 뽑아 주는 메서드 , 
+
+	// 랜덤하게 번호를 하나 뽑아 주는 메서드 ,
 	public static int generateRandomNumber() {
 		int randomNumber = (int) (Math.random() * 45 + 1);
 		return randomNumber;
-		
+
 	}
-	
-	// 배열에서, 숫자가 중복이 되는지 여부를 확인하는 메서드. 
-//	public static boolean checkNumber(int number, int[] array) {
-//		boolean numCheck = false;
-//		for(int i = 0 ; i<array.length; i++) {
-//			if(number == array[i]) {
-//				numCheck = true;
-//				break;
-//			}
-//			
-//		}
-//		
-//	}
+
+	// 배열에서, 숫자가 중복이 되는지 여부를 확인하는 메서드.
+	public static boolean checkNumber(int number, int[] array) {
+		boolean numCheck = false;
+		for (int i = 0; i < array.length; i++) {
+			if (number == array[i]) {
+				numCheck = true;
+				break;
+			}
+
+		}
+		return numCheck;
+
+	}
 
 }
-
-
-
