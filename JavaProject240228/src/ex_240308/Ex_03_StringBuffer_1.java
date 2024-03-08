3462e6e758fbf708a9aa0a30e276e6eec33b23f5
lsy3709 <lsy3709@naver.com>
2024-03-08 12:39:14 +0900

    4교시 StringBuffer 확인 2

--- JavaProject240228/src/ex_240308/Ex_03_StringBuffer_1.java
+++ JavaProject240228/src/ex_240308/Ex_03_StringBuffer_1.java
@@ -15,6 +15,39 @@ public class Ex_03_StringBuffer_1 {
 		System.out.println("=======str3,str4======합친 문자열의 메모리 위치값==");
 		System.out.println("str3Concat System.identityHashCode : " + System.identityHashCode(str3Concat));
 		System.out.println("str4Concat System.identityHashCode : " + System.identityHashCode(str4Concat));
+		
+		System.out.println("==========================");
+		// 문제점을 개선한, StringBuffer를 활용해보기. 
+		// 테스트 : 기존 문자열에 붙이는 concat 메서드 이용해서. 
+		// 기존 사용하던 메모리 위치 주솟값을 계속 활용 하는지 여부.
+		StringBuffer str1 = new StringBuffer("HELLO");
+		
+		System.out.println("HELLO 에 뒤에 붙이기 전 주소값 : 확인");
+		System.out.println("str1 "
+				+ "System.identityHashCode: " + System.identityHashCode(str1));
+		
+		str1.append(" world");
+	
+		
+		System.out.println("str1.append(\" world\") : " + str1);
+		System.out.println("HELLO 에 뒤에 붙이기 후 주소값 : 확인");
+		System.out.println("str1.append(\" world\")  "
+				+ "System.identityHashCode: " + System.identityHashCode(str1));
+		
+		
+		// 메서드 기능 간단히 확인.
+		// 012345678910
+		// HELLO world
+		str1.insert(5, " my");
+		// StringBuffer에 따로 toString이 정의가 되어서, 바로 인스턴스 변수로 출력이 가능함. 
+		System.out.println("str1.insert(5, \" my\") : " + str1);
+		
+		// 결과 문자열 
+		// 01234567891234
+		// HELLO my world
+		
+		str1.replace(1, 5, "i");
+		System.out.println("str1.replace(1, 5, \"i\") : " + str1);
 
 	}
 
