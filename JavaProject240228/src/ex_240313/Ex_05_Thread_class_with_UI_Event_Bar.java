e6b9d8c45247ed89404440d769a1e9018fd2377a
lsy3709 <lsy3709@naver.com>
2024-03-13 15:27:57 +0900

    6교시 스레드 기본 4, Swing UI 같이, 이벤트 핸들로 키입력 막대 채우기 3

--- JavaProject240228/src/ex_240313/Ex_05_Thread_class_with_UI_Event_Bar.java
+++ JavaProject240228/src/ex_240313/Ex_05_Thread_class_with_UI_Event_Bar.java
@@ -1,12 +1,18 @@
 package ex_240313;
 
 import java.awt.Color;
+import java.awt.Container;
 import java.awt.Graphics;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
-// 화면 UI 담당 클래스 
-class MyLabel extends JFrame {
+// 화면 UI 담당 클래스
+// 막대만 그리는 클래스.
+// 실제 전체 창 등은, 메인에서 그리기. 
+class MyLabel extends JLabel {
 	// JFrame : 자바 버전의 화면 그리는 도구 모음집.
 	// 막대 바의 수치를 담을 변수, 전체를 100
 	// 해당 막다의 수치가 커질수록, 분홍색(마젠타색)으로 채울 예정.
@@ -121,11 +127,74 @@ class ConsumerThread extends Thread {
 }
 
 // 실행용 클래스 사용1
-public class Ex_05_Thread_class_with_UI_Event_Bar {
+//  부모의 윈도우 창 의미, 
+// MyLabel -> 막대 그림만 의미. 
+// 결론, MyLabel을 윈도우 창에 붙이기 예정. 
+public class Ex_05_Thread_class_with_UI_Event_Bar extends JFrame {
+	// 사용할 막대 인스턴스를 생성. 최대 크기는 임의로 100으로 잡기. 
+	private MyLabel bar = new MyLabel(100);
 	
+	// 실행할 클래스에서 생성자 호출. 
+	Ex_05_Thread_class_with_UI_Event_Bar(){
+		setTitle("분홍색 막대 바 채우기 스레드 동기화 이용, wait, notify 사용하기. ");
+		// 창 닫기 기능. 
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		// 패널 요소를 배치(정렬방법)
+		Container container = new Container();
+		// 요소의 정렬 기능이 없다.
+		// 여기서는 단순히, 막대바 하나로 확인중이라서, 
+		container.setLayout(null);
+		// 기본 막대의 배경색 : 오렌지 색.
+		bar.setBackground(Color.ORANGE);
+		// JLabel 의 기본이 투명도가 있어서, 추가로 메서드 호출을해서 지정.
+		// 배경색 적용. 
+		bar.setOpaque(true);
+		// 창으로 부터, 가로 20만큼 떨어지고, 50 만큼 아래로 떨어지기
+		bar.setLocation(20,50);
+		bar.setSize(300,20);
+		
+		//창에 막대 붙이기 작업. 
+		container.add(bar);
+		
+		// 이벤트 핸들러 추가하기. 
+		// 키 입력시, 채우는 기능 넣기.
+		// new KeyAdapter() { : 익명 클래스, 이름 없는 클래스 이고, 
+		// 주로, 1회용으로 사용시 이용. 보통) 이벤트 핸들러 많이 사용함. 
+		container.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				super.keyPressed(e);
+				// 기본 배경인 오렌지색 막대에, 키 입력으로 , 분홍색 막대를 1씩 채우는 역할. 
+				bar.fill();
+			}
+		});
+		
+		// 부모의 창 크기. 
+		setSize(350,200);
+		// 창 보이기 옵션. 
+		setVisible(true);
+		
+		//부모 창에서 옵션, 포커스 
+		// 요소의 집중. 입력란에 커서 깜박이는 것처럼, 해당 요소를 가리키고 있다. 
+		container.setFocusable(true);
+		// 막대가 그려질 때마다, 해당 창을 봐주세요. 
+		container.requestFocus();
+		
+		// 스레드 동작 시키기 
+		ConsumerThread consumerThread = new ConsumerThread(bar);
+		// 스레드 호출 
+		consumerThread.start();
+	}
 
 	public static void main(String[] args) {
-
+		// 메인 클래스의 기본 생성자 호출을 바로 함. 
+		// 원래는 인스턴스 명에 담아서, 호출,
+		// 방법1
+		 new Ex_05_Thread_class_with_UI_Event_Bar();
+		// 방법 2
+//		Ex_05_Thread_class_with_UI_Event_Bar test = 
+//				new Ex_05_Thread_class_with_UI_Event_Bar();
+		
 
 	}
 
