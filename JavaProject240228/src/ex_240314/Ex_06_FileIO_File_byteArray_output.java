1ef59d25caf46947c3421148df3b6cff002c975a
lsy3709 <lsy3709@naver.com>
2024-03-14 12:47:25 +0900

    4교시 jdbc 준비물 작업중1

--- JavaProject240228/src/ex_240314/Ex_06_FileIO_File_byteArray_output.java
+++ JavaProject240228/src/ex_240314/Ex_06_FileIO_File_byteArray_output.java
@@ -5,13 +5,20 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 
 public class Ex_06_FileIO_File_byteArray_output {
+	
+	// 활용은, 원본 이미지를 그대로 사용 안하고, 나중에 썸네일(작은 이미지), 
+	// 특정 크기 맞게, 콤프레스 압축해서 재사용할 때 이용을 많이 함. 
+	// 바이트로 읽어서 예 : 이미지, 동영상, 한글 파일, 엑셀 파일, 등, 문자가 아닌 모든 파일이 
+	// 다 활용이 됨.
 
 	public static void main(String[] args) {
 		// 이미지 복사, 바이트로 읽어서, 쓸 때, 한바이트가 아니라, 특정 배열 크기 만큼씩 쓰기. 
 		// 원본 이미지 경로, 출력 대상 경로, File 클래스 이용하기. 
-		File src = new File("c:\\temp_240314\\브레드1.jpg");
+//		File src = new File("c:\\temp_240314\\브레드1.jpg");
+		File src = new File("c:\\temp_240314\\testVideo.mp4");
 		// 복사한 이미지 파일(여기에, 출력할 예정. )
-		File dst = new File("c:\\temp_240314\\복사된_브레드1.jpg");
+//		File dst = new File("c:\\temp_240314\\복사된_브레드1.jpg");
+		File dst = new File("c:\\temp_240314\\testVideo2.mp4");
 		
 		try {
 			// 이미지를 읽으려면 바이트로 읽어야 한다. 바이트를 읽기 위한 도구. 
---
+++ JavaProject240228/src/ex_240314/jdbcTest/Ex_01_jdbc_test_1.java
@@ -0,0 +1,21 @@
+package ex_240314.jdbcTest;
+
+public class Ex_01_jdbc_test_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 준비물 
+		// 1) 본인 자리에 오라클 디비 서버 가 설치 유무
+		// 2) sql developer 설치 되었는지
+		// 3) 연결할 아이디 확인. id : system , pw : oracle
+		
+		// jdbc 드라이버 , 프로젝트에 적용하는 방법. 
+		// 4) ojdbc8_g.jar , 파일을 본인 이클립스 패키지에 복사하고
+		// 5) 자바 프로젝트 우클릭 -> build path -> 마지막 메뉴 선택 
+		// 6) 3번째 탭 라이브러리즈에서, 2번째 classpath에 -> add jar 선택 하고
+		// 7) 해당 자바 프로젝트 -> 패키지-> ojdbc8_g.jar 선택하기. 
+		
+
+	}
+
+}
