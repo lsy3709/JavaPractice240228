b2f0fb4398c36e47f5f33269da5549d22c6792bd
lsy3709 <lsy3709@naver.com>
2024-03-14 12:10:45 +0900

    4교시 FileIO 기본 6, File 클래스의 기본 메서드 확인.3

--- JavaProject240228/src/ex_240314/Ex_05_FileIO_File_method.java
+++ JavaProject240228/src/ex_240314/Ex_05_FileIO_File_method.java
@@ -35,7 +35,7 @@ public class Ex_05_FileIO_File_method {
 		// 파일 생성, 폴더 생성, 파일의 메타 정보등을 확인 가능. 
 		// 직접적인 입출력 불가. 
 		
-		File f1 = new File("c:\\windows\\system.in");
+		File f1 = new File("c:\\windows\\system.ini");
 		File f2 = new File("c:\\temp_240314");
 		// 출력 테스트 
 		listDirectory(f2);
@@ -46,7 +46,26 @@ public class Ex_05_FileIO_File_method {
 		// 내가 사용한 파일외에, 시스템에 제공하는 기본 파일등이 있음. 
 		// 그래서, 결론, 오류코드가 보이면, 먼저) 내가 작업한 파일이 있는지 여부를 확인. 
 		
+		System.out.println("======================================");
+		// 파일인지, 디렉터리 인지 유무 확인, 디렉터리 생성도 확인. 
+		if (f1.isFile()) {
+			System.out.println("파일입니다.");
+		} else {
+			System.out.println("폴더입니다.");
+		}
 		
+		// exist(): 존재 유무 확인, mkdir() : 폴더 생성 
+		File f3 = new File("c:\\temp_240314");
+		if(f3.isDirectory()) {
+			if(f3.exists()) {
+				File f4 = new File("c:\\temp_240314\\TestFolder");
+				f4.mkdir();
+				// 만들고 나서 삭제 확인 해보기. 
+				//f4.delete();
+				// renameTo() : 이름변경 
+				f4.renameTo(new File("c:\\temp_240314\\TestFolder2"));
+			}
+		}
 
 	}
 
