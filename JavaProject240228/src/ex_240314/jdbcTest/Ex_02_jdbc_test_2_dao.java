b6db4f521cdda72478774e1680a50be20d41c6fc
lsy3709 <lsy3709@naver.com>
2024-03-14 15:40:57 +0900

    6교시 JDBC 기본 1, select 조회 연습 순서 붙이기4, 조회 기능 분리3

--- JavaProject240228/src/ex_240314/jdbcTest/Ex_02_jdbc_test_2_dao.java
+++ JavaProject240228/src/ex_240314/jdbcTest/Ex_02_jdbc_test_2_dao.java
@@ -18,71 +18,89 @@ public class Ex_02_jdbc_test_2_dao {
 	private final static String USER_ID = "system";
 	private final static String USER_PW = "oracle";
 	
+	static // 연결하기위한 인스턴스 , 고정
+	Connection con = null;
+	// 쿼리를 전달 하기 위한 인스턴스 , 고정
+	static PreparedStatement pstmt = null;
+	// 만약 , 조회를 한다면, 조회한 결과를 담을 테이블 같은 인스턴스, : 조회시 만 필요.
+	// insert, update, delete 시에는 안씀. , 고정
+	static ResultSet rs = null;
+
 	// 조회하는 기능
 	public static void select() throws ClassNotFoundException, SQLException {
-		// 연결하기위한 인스턴스 , 고정
-				Connection con = null;
-				// 쿼리를 전달 하기 위한 인스턴스 , 고정
-				PreparedStatement pstmt = null;
-				// 만약 , 조회를 한다면, 조회한 결과를 담을 테이블 같은 인스턴스, : 조회시 만 필요.
-				// insert, update, delete 시에는 안씀. , 고정
-				ResultSet rs = null;
+		// select 조회 하기위한 순서
 
-				// select 조회 하기위한 순서
-
-				try {
-					// 순서1, 고정
-					// 드라이버 , 메모리에 올려야함. 로드
-					// 해당 패키지 읽기 위해서는 반드시 강제로, try catch 하라고 함.
-					Class.forName(DRIVER);
+		try {
+			// 순서1, 고정
+			// 드라이버 , 메모리에 올려야함. 로드
+			// 해당 패키지 읽기 위해서는 반드시 강제로, try catch 하라고 함.
+			Class.forName(DRIVER);
 //					System.out.println("Class.forName(DRIVER) 실행 후");
 
-					// 순서2 , 고정
-					// 연결하기위 한 Connection 타입의 con 을 초기화하기.
-					con = DriverManager.getConnection(URL, USER_ID, USER_PW);
+			// 순서2 , 고정
+			// 연결하기위 한 Connection 타입의 con 을 초기화하기.
+			con = DriverManager.getConnection(URL, USER_ID, USER_PW);
 //					System.out.println("DriverManager.getConnection(URL, USER_ID, USER_PW) 실행 후");
 
-					// 순서3 , 값 변경, select, insert, update, delete
-					// 지금 조회 연습 중이니, 조회하는 sql 전달 명령어 준비하기.
-					String query = "SELECT id,pwd,name FROM test_java";
+			// 순서3 , 값 변경, select, insert, update, delete
+			// 지금 조회 연습 중이니, 조회하는 sql 전달 명령어 준비하기.
+			String query = "SELECT id,pwd,name FROM test_java";
 
-					// 순서4 , 해당 쿼리를 전달하는 PreparedStatement(pstmt) 초기화하기.
-					pstmt = con.prepareStatement(query);
+			// 순서4 , 해당 쿼리를 전달하는 PreparedStatement(pstmt) 초기화하기.
+			pstmt = con.prepareStatement(query);
 //					System.out.println("con.prepareStatement(query) 실행 후");
 
-					// 순서5, DB에서 조회가 된 결과를 테이블에 담아 둔다.
-					// 테이블 역할을 하는 ResultSet (rs)초기화하기.
-					// 메모리 상에 조회된 데이터가 있다.
-					// 바로 볼수 없으니, 우리가 보기 편하게 재가공
-					// 현재) 자바 1) 콘솔 2) 스윙UI 3) HTML(웹)
-					rs = pstmt.executeQuery();
+			// 순서5, DB에서 조회가 된 결과를 테이블에 담아 둔다.
+			// 테이블 역할을 하는 ResultSet (rs)초기화하기.
+			// 메모리 상에 조회된 데이터가 있다.
+			// 바로 볼수 없으니, 우리가 보기 편하게 재가공
+			// 현재) 자바 1) 콘솔 2) 스윙UI 3) HTML(웹)
+			rs = pstmt.executeQuery();
 
-					// 순서6, 반복문으로 데이터를 조회해보기.
-					// rs.next() -> 기준이 0행부터 시작해서, 다음 행인 1행이 있는지 확인함..
+			// 순서6, 반복문으로 데이터를 조회해보기.
+			// rs.next() -> 기준이 0행부터 시작해서, 다음 행인 1행이 있는지 확인함..
 //					System.out.println("while (rs.next()) { 실행 전");
-					while (rs.next()) {
-						String user_id = rs.getString("id");
-						String user_pwd = rs.getString("pwd");
-						String user_name = rs.getString("name");
-						System.out.println("ID: " + user_id + ", PWD: " + user_pwd + ", NAME: " + user_name);
-					}
+			while (rs.next()) {
+				String user_id = rs.getString("id");
+				String user_pwd = rs.getString("pwd");
+				String user_name = rs.getString("name");
+				System.out.println("ID: " + user_id + ", PWD: " + user_pwd + ", NAME: " + user_name);
+			}
 
-				} catch (SQLException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} finally {
-					// 순서7, 자원반납, 사용의 역순 ,
-					// 사용순서 1) con 2) pstmt 3) rs
-					rs.close();
-					pstmt.close();
-					con.close();
-				}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			// 순서7, 자원반납, 사용의 역순 ,
+			// 사용순서 1) con 2) pstmt 3) rs
+			rs.close();
+			pstmt.close();
+			con.close();
+		}
 	}
-	
-	// insert 하는 기능, 매개변수 ( id, pwd, name ) 받아서 전달할 예정. 
-	
-	// delete 하는 기능, 해당 id를 받아서 삭제할 예정. 
-	
-	// update 는 id 제외하고, pwd , name 만 변경하기. 
+
+	// insert 하는 기능, 매개변수 ( id, pwd, name ) 받아서 전달할 예정.
+	public static void insert(String id, String pwd, String name) throws ClassNotFoundException, SQLException {
+		// 순서1 ~ 순서7 반복.
+		// 순서1
+		Class.forName(DRIVER);
+		// 순서2
+		con = DriverManager.getConnection(URL, USER_ID, USER_PW);
+		// 순서3 , insert 이부분 조금 다름. -> 수정 할 예정. 
+		String query = "SELECT id,pwd,name FROM test_java";
+		// 순서4 
+		pstmt = con.prepareStatement(query);
+		// 순서5 
+		int resultNum = pstmt.executeUpdate();
+		System.out.println("레코드가 " + resultNum + "개 저장되었습니다.");
+		
+		// 순서6, select 할 때 사용. 
+		// 순서7, 자원반납. 
+		
+	}
+
+	// delete 하는 기능, 해당 id를 받아서 삭제할 예정.
+
+	// update 는 id 제외하고, pwd , name 만 변경하기.
 
 }
--- JavaProject240228/src/ex_240314/jdbcTest/Ex_02_jdbc_test_2_main.java
+++ JavaProject240228/src/ex_240314/jdbcTest/Ex_02_jdbc_test_2_main.java
@@ -1,9 +1,20 @@
 package ex_240314.jdbcTest;
 
+import java.sql.SQLException;
+
 public class Ex_02_jdbc_test_2_main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		try {
+			// 분리된 조회 기능 호출 해보기. 
+			Ex_02_jdbc_test_2_dao.select();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
