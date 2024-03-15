f08cf2a475fd077c9a0dc556eb128efd562a2438
lsy3709 <lsy3709@naver.com>
2024-03-15 15:06:17 +0900

    6교시 멀티채팅 기본 설명 달기. 회원관리 참고해서 게시판 만들기

--- JavaProject240228/src/ex_240311_socket_chat/single_multi_chat/ServerBackground.java
+++ JavaProject240228/src/ex_240311_socket_chat/single_multi_chat/ServerBackground.java
@@ -22,6 +22,10 @@ public class ServerBackground {
     private ServerGUI gui;
     private String msg;
     /** XXX 03. 사용자들의 정보를 저장하는 맵 */ 
+    // 키 : 닉네임, 채팅방 입장시 사용했던 이름 : 예) 이상용 
+    // 값 : DataOutputStream, 소켓을 이용해서, 상대방이 입력받은 내용을 출력을 해주는 기능. 
+    // clientMap : 모든 사용자의 이름과, 해당 이름에 대한 출력을 하는 기능인 인스턴스가 들어가 있음. 
+    
     private Map<String, DataOutputStream> clientMap = new HashMap<String, DataOutputStream>();
  
     public void setGui(ServerGUI gui) {
@@ -36,17 +40,23 @@ public class ServerBackground {
     public void setting() {
         
         try {
-            Collections.synchronizedMap(clientMap); //교통정리를 해준다.( clientMap을 네트워크 처리해주는것 ) 
+        	// 스레드인데, 작업 스케줄, 순서 맞게 , 동시 실행이 가능하게 해주는 역할. 
+            Collections.synchronizedMap(clientMap); //교통정리를 해준다.( clientMap을 네트워크 처리해주는것 )
+            // 서버가 해당 포트로 소켓을 이용해서, 통신 준비. 
             serverSocket = new ServerSocket(7777);
  
             while (true) {
                 /** XXX 01.서버가 할일 : 방문자를 계속 받아서, 쓰레드 리시버를 계속 생성 */
                 
                 System.out.println("대기중.....");
+                // 소켓이 , 클라이언트가 요청을하면 수락을 하는 기능, 
+                // 수락을하면 , 서버 <-->클라이언트 , 연결고리:소켓
                 socket = serverSocket.accept(); // 여기서 클라이언트 받음
+                // socket 의 여러 기능중 하나이고, getInetAddress, 정보를 출력해줌. 
                 System.out.println(socket.getInetAddress() + "에서 접속했습니다.");
                 
                 //여기서 새로운 사용자 스레드 클래스를 생성해서 소켓 정보를 넣어줘야한다.
+                // 스레드 클래스 
                 Receiver receiver = new Receiver(socket);
                 receiver.start();
             }
@@ -59,8 +69,11 @@ public class ServerBackground {
     //맵의내용(클라이언트) 저장과 삭제 
     public void addClient(String nick, DataOutputStream out) throws IOException{
         String message=nick + "님이 접속하셨습니다.\n";
+        // 모든 유저에게 전달.
         sendMessage(message);
+        // 메세지를 본인의 채팅화면에 붙이고
         gui.appendMsg(message);
+        // 해시맵에 인스턴스를 등록하기.(요소등록)
         clientMap.put(nick, out);
         
     }
@@ -72,14 +85,33 @@ public class ServerBackground {
         clientMap.remove(nick);
     }
     
-    //메세지 내용 전파 
+    //메세지 내용 전파 , 브로드캐스트(방송), 방송국 방송 전파 <-> 시청자들 모두 볼수 있음. 
+    // 메세지 내용을 받으면, 
+    // 메세지을 , 맵에 모든 유저가 들어가 있고, 해당 키에 대한 값으로, 
+    // 그 유저에게 메세지를 쓰는 인스턴스가 있는데, 
+    // 이 인스턴스를 이용해서, 메세지 모두 전판 하는 기능. 
     public void sendMessage (String msg){
+    	//Iterator: 반복이 가능한 타입, 
+    	// 보통은 해시맵에서, 해당 키에 대한 정보와 값을 모두 받습니다. 
+    	// 현재는, keySet 키의 정보만 받았음. 
         Iterator<String> iterator = clientMap.keySet().iterator(); //key셋으로 반복자지정
+        // 현재, 키에는 , 닉네임이 들어가 있음. 
         String key = "";
         
+        // 모든 키값을 순회해서 확인,
+        // 결론, 모든 닉네임에 해당하는 출력 기능 인스턴스를 이용해서, 출력을 할 예정. 
         while(iterator.hasNext()){
+        	// 맵 , 키: 이상용, 값 : 출력기능 인스턴스
+        	// 맵 , 키: 상준, 값 : 출력기능 인스턴스
+        	// 맵, 키 : 서현, 값 : 출력기능 인스턴스
+        	// ... , 모든 멤버가 해당 맵안에 다 들어가 있다. 
+        	
+        	// 키 : 이상용, 키2 : 상준, 키3: 서현.. 
             key = iterator.next();// 반복자에서 하나하나 키를 빼온다.
             try{
+            	// 이상용 키에 대한get(key) 값을 가지고 옵니다.
+            	// writeUTF, 쓰기 작업 , 출력을 , 채팅 메세지 내용. 
+            	// writeUTF , 한글 안깨지기 위해서 사용. 
                 clientMap.get(key).writeUTF(msg);
             } catch(IOException e){
                 e.printStackTrace();
@@ -87,17 +119,30 @@ public class ServerBackground {
         }
     }
     // ------------------리시버---------------------------
+    // 스레드 정의 1) Thread 상속 2) Runnable 인터페이스를 구현
     class Receiver extends Thread {
         /** XXX 리시버가 할일 : 네트워크 소켓을 받아서 계속듣고 보내는 일. */
+    	// 데이터, 메세지를 전달하는 역할. 
         private DataInputStream in; // 데이터 입력 스트림
         private DataOutputStream out; // 데이터 아웃풋 스트림
         private String nick;
- 
+        
+        // 생성자, Socket 타입의 인스턴스를 받고 있다. 
+        // 유저가 1) 맵에 등록이되고, 2) 스레드를 상속받은 클래스를 생성 3) 해당 스레드 클래스에 소켓 인스턴스전달.
+        // Socket , 서버, 클라이언트의 연결고리 
         public Receiver(Socket socket) {
             try {
+            	// socket.getOutputStream() : 출력을 담당하기. , 기반 스트림. 
+            	// new DataOutputStream : 보조 스트림
+            	// 출력 담당, 예) 유저 가 입력한 채팅의 내용을 출력하는 기능. 
                 out = new DataOutputStream(socket.getOutputStream());
+                // 입력 담당 , 예) 유저가 입력한 내용을 입력으로 받아들이는 기능. 
                 in = new DataInputStream(socket.getInputStream());
+                // 실행후, 닉네임 : 이상용, 이상용을 읽을 때, 한글 깨짐 없게 할려고, 
+                // 타입을 UTF 했음. 
                 nick = in.readUTF();
+                // 닉네임을 입력 받았고, 출력을 담당하는 기능과 같이해서, 맵에 담기. 
+                //
                 addClient(nick,out);
             } catch (IOException e) {
                 e.printStackTrace();
@@ -108,9 +153,17 @@ public class ServerBackground {
         public void run() {
  
             try {
+            	// in이 null 이 아니면, 계속 동작을함. 
+            	// in이 null 이면, 반복 멈춤. 
+            	// in 자원 반납을 하지 않는 이상, 계속 동작을 한다. 
+            	// 종료시에 자원을 반납합니다. ->이때 in null 이됨.  
                 while (in != null) {
+                	// 유저가 입력한 채팅의 내용을 한글 깨짐 방지 하기위해
+                	// 중간 데이터 타입인 UTF 
                     msg = in.readUTF();// UTF로 읽어들인다.
+                    // 다른 유저들에게 전달. 
                     sendMessage(msg);
+                    // 본인의 창에 붙이기 작업. 
                     gui.appendMsg(msg);
                 }
             } catch (Exception e) {
--- JavaProject240228/src/ex_240311_socket_chat/single_multi_chat/ServerGUI.java
+++ JavaProject240228/src/ex_240311_socket_chat/single_multi_chat/ServerGUI.java
@@ -36,6 +36,7 @@ public class ServerGUI extends JFrame implements ActionListener {
 	// 우리는 서버의 기능을 이용이 가능함. 
 	private ServerBackground server = new ServerBackground();
 
+	// 디폴트 생성자. 서버 클래스 인스턴스 생성시 반드시 실행이 되는 부분임. 
 	public ServerGUI() throws IOException {
 		//현재 , 보더 레이아웃, 배치관리자, 요소를 정렬를 어떻게 할거니, 
 		// 동 서 남 북 가운데, 정렬이 가능한 배치 관리자. 
@@ -54,35 +55,65 @@ public class ServerGUI extends JFrame implements ActionListener {
 //		container.add(new JScrollPane(jta));
 		
 		// 보더 레이아웃 
-		container.setLayout(new FlowLayout());
+		container.setLayout(new BorderLayout());
 		container.add(jta,BorderLayout.CENTER);
 		container.add(jtf,BorderLayout.SOUTH);
 		container.add(new JScrollPane(jta),BorderLayout.CENTER);
-
+		
+		// 이벤트 핸들러
+		// 현재 클래스가 , ActionListener 인터페이스를 구현하고 있어서, 
+		// 추상메스드를 이용해서 사용가능.
+		// this -> 현재 클래스 의미. 
+		// 구현 한 인터페이스에 정의된 메서드 명 : actionPerformed, 우리는 반드시 재정의해야 함. 
 		jtf.addActionListener(this);
-
+		// 부모창을 종료시 설정하는 기능. 
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		// 부모창을 보여주기. 
 		setVisible(true);
+		// 부모창의 위치는 윈도우즈 창으로부터 x 200 만큼, y 100 만큼 이동하고, 
+		// 창의 길이는 가로 400, 높이 600
 		setBounds(200, 100, 400, 600);
+		// 창의 제목 
 		setTitle("서버부분");
+		// server : 백그라운드의 인스턴스에서, 
+		// 해당 인스턴스의 멤버중에, 현재 작업하는 클래스 이름 : ServerGUI
+		// 멤버로 가지고 있고, setGui 세터를 통해서, 멤버를 업데이트함. 
 		server.setGui(this);
+		
+		// ServerGUI -> 화면 부분 설명 후 , 다시 돌아와서 설명하기. 
+		
 		server.setting(); // GUI가 실행될때 서버도 같이 동작하도록..
 	}
 
 	public static void main(String[] args) throws IOException {
+		// 해당 클래스를 실행하게 되면, 
+		// 본인의 디폴트 생성자를 호출 하면서, 실행이됨. 
+		// 화면도 그리면서, 백그라운드도 같이 동작을 합니다. 
+		// 왜? 현재 클래스에는 본인의 화면을 그리는 업무 외, 서버의 인스턴스를 가지고 있어서, 
+		// 해당 기능을 사용이 가능해서, 
 		new ServerGUI();
 	}
 
+	// jtf 의 이벤트 핸들러가 동작시, 수행하는 메서드 부분. 
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		//  jtf.getText(), 입력창에 입력된 메세지 가져오기(게터)
 		String msg = "서버 : " + jtf.getText() + "\n";
+		// 본인 콘솔에 출력하고, 
 		System.out.print(msg);
+		// 상대방(클라이언트)들에게 메세지 전달하는 기능. 
 		server.sendMessage(msg);
+		// 현재 본인 창에(서버창) 내용을 붙이기. 
 		appendMsg(msg);
+		// 메세지 입력 후에는 , 입력란 비우기. 
+		
+		// 추후 계획, 이 대화의 내용을 데이터베이스 저장하면 어떻까?
+		
 		jtf.setText("");
 	}
 
 	public void appendMsg(String msg) {
+		// 채팅창에 해당 메세지 붙이기. 
 		jta.append(msg);
 	}
 }
